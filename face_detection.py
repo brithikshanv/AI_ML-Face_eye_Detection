8bb6e0df06f3f1a37997b12097d0d36487dc708d
brithiksha nv <136951676+brithikshanv@users.noreply.github.com>
2024-07-09 11:14:20 +0530

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,44 @@
+import cv2
+
+# Load the cascade classifier
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
+
+# Initialize camera
+camera = cv2.VideoCapture(0)
+
+# Check if camera opened successfully
+if not camera.isOpened():
+    print("Error: Unable to open camera.")
+    exit()
+
+
+# Loop to capture frames from the camera
+while True:
+    # Capture frame-by-frame
+    ret, frame = camera.read()
+
+    # Check if frame is read correctly
+    if not ret:
+        print("Error: Unable to read frame.")
+        break
+
+    # Convert frame to grayscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    # Detect faces in the grayscale frame
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    # Draw rectangles around the detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+    # Display the resulting frame
+    cv2.imshow('Face Detection', frame)
+
+    # Break the loop if 'ESC' key is pressed
+    if cv2.waitKey(1) == 27:
+        break
+
+# Release the camera and close all windows
+camera.release()
+cv2.destroyAllWindows()
